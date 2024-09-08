eda14d3e081f3866ef443a9a1057aded7674c3c7
jawwad-ali <alijawwad001@gmail.com>
2024-09-08 18:45:50 +0500

    class06.py

---
+++ class06.py
@@ -0,0 +1,71 @@
+# budget = 4580
+
+# Code 1
+# if budget <= 500:
+#     print("Eat chicken Roll!")
+# else:
+#     print("Eat Kababjees Highway")
+
+
+
+# Code 2
+# age = 17.99
+# if age >=18:
+#     print("You can make your CNIC")
+# else:
+#     print("You are not eligible")
+
+
+# Code 3
+# email = "ali@gmail.com"
+# passcode = 123456
+
+
+# if email == "ali@gmail.com" and passcode == 1234567:
+#     print("You are logged In")
+    
+# else:
+#     print("invalid creds")
+
+
+
+# Else if
+percentage = 33.333
+
+
+if percentage >= 90:
+    print("A+ Grade")
+
+elif percentage >= 80:
+    print("B Grade")
+
+elif percentage >= 70:
+    print("C Grade")
+    
+elif percentage >= 50:
+    print("D Grade")
+
+else:
+    print("Fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
