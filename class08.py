0d12576758f9538c4d59aaf2d4e3f337bd37114e
jawwad-ali <alijawwad001@gmail.com>
2024-09-14 20:45:26 +0500

    add class08.py lists/arrays

---
+++ class08.py
@@ -0,0 +1,33 @@
+# Arrays/list
+# std1 = "Ali"
+# std2 = "mustafa"
+# std3 = "asif"
+
+# std500 = "hasnain"
+
+
+NightGathering = [
+    "Ali Jawwad", "Mustafa",  "Asif", "Hasnain"
+]
+
+
+print(NightGathering)
+
+print(NightGathering[0])
+print(NightGathering[1])
+print(NightGathering[2])
+print(NightGathering[3])
+
+# Add value in end of the array
+# print(NightGathering.append("Maisum"))
+
+# print(NightGathering)
+
+print(NightGathering.remove("Ali Jawwad"))
+print(NightGathering)
+
+print(NightGathering.insert(1,"Akram"))
+print(NightGathering)
+
+print(NightGathering.pop(3))
+print(NightGathering)
