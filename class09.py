35681caf804a73f30c801a2ad4992aff11792ea4
jawwad-ali <alijawwad001@gmail.com>
2024-09-21 20:44:42 +0500

    add class09.py

---
+++ class09.py
@@ -0,0 +1,41 @@
+studentData = {
+    "studetName": "Ali Jawwad",
+    "age": 23,
+    "email": "ali@gmail.com",
+    "rollNo": 88319,
+    "isMarried": False
+}
+# print(studentData)
+
+print(studentData["age"])
+print(studentData["studetName"])
+print(studentData["isMarried"])
+
+
+
+
+
+
+
+
+studentInfo = [
+    {
+        "studentName":"Ali",
+        "age":23
+    }, 
+    {
+        "studentName":"Mustafa",
+        "age":30
+    },
+    {
+        "studentName":"Asif",
+        "age":44
+    }
+]
+# print(studentInfo)
+print(studentInfo[0]["studentName"])
+
+
+
+
+
